ee7875e06d466aa95fab893f357aa4b830f8fadc
fanboynz <mp3geek@gmail.com>
2019-10-27 11:36:33 +0100

    revert regex

--- functions/sendLeagueChampionEmbed.js
+++ functions/sendLeagueChampionEmbed.js
@@ -1,13 +1,12 @@
 module.exports = function (message, name, title, description, tags, roles) {
     const discord = require("discord.js");
-	var loldomain = /^https?:\/\/.*leagueoflegends\.com\/cdn\/\d.\d.*\/img\/champion\//;
     
     var lolchampEmbed = new discord.RichEmbed()
     .setColor('#e74999')
     .setTitle(name + " - " + title)
     .setURL('https://na.leagueoflegends.com/en/game-info/champions/' + name.toLowerCase().replace(' ', '').replace("'", "") + '/')
     .setDescription(description)
-    .setThumbnail(loldomain + name + '.png')
+    .setThumbnail('https://ddragon.leagueoflegends.com/cdn/9.21.1/img/champion/' + name + '.png')
     .addField('Classes', tags, true)
     .addField('Roles', roles, true)
     .addField('Builds', ['https://app.mobalytics.gg/champions/' + name.toLowerCase().replace(' ', '').replace("'", "") + '/build','https://champion.gg/champion/' + name.toLowerCase().replace(' ', '').replace("'", ""),'https://u.gg/lol/champions/' + name.toLowerCase().replace(' ', '').replace("'", "") + '/build'])
