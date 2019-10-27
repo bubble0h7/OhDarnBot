61aa1c9d9cc011540dae0b93220f1f7df25ce21b
fanboynz <mp3geek@gmail.com>
2019-10-27 09:16:01 +0100

    Update domain

--- functions/sendLeagueChampionEmbed.js
+++ functions/sendLeagueChampionEmbed.js
@@ -6,7 +6,7 @@ module.exports = function (message, name, title, description, tags, roles) {
     .setTitle(name + " - " + title)
     .setURL('https://na.leagueoflegends.com/en/game-info/champions/' + name.toLowerCase().replace(' ', '').replace("'", "") + '/')
     .setDescription(description)
-    .setThumbnail('https://ddragon.leagueoflegends.com/cdn/9.15.1/img/champion/' + name + '.png')
+    .setThumbnail('https://ddragon.leagueoflegends.com/cdn/9.21.1/img/champion/' + name + '.png')
     .addField('Classes', tags, true)
     .addField('Roles', roles, true)
     .addField('Builds', ['https://app.mobalytics.gg/champions/' + name.toLowerCase().replace(' ', '').replace("'", "") + '/build','https://champion.gg/champion/' + name.toLowerCase().replace(' ', '').replace("'", ""),'https://u.gg/lol/champions/' + name.toLowerCase().replace(' ', '').replace("'", "") + '/build'])
