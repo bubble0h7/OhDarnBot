cb39cf7f6f48086194b1631b3eba07abffdbe2ec
fanboynz <mp3geek@gmail.com>
2019-10-27 10:53:31 +0100

    Allow regex check

--- functions/sendLeagueChampionEmbed.js
+++ functions/sendLeagueChampionEmbed.js
@@ -1,12 +1,13 @@
 module.exports = function (message, name, title, description, tags, roles) {
     const discord = require("discord.js");
+	var loldomain = /^https?:\/\/.*leagueoflegends\.com\/cdn\/\d.\d.*\/img\/champion\//;
     
     var lolchampEmbed = new discord.RichEmbed()
     .setColor('#e74999')
     .setTitle(name + " - " + title)
     .setURL('https://na.leagueoflegends.com/en/game-info/champions/' + name.toLowerCase().replace(' ', '').replace("'", "") + '/')
     .setDescription(description)
-    .setThumbnail('https://ddragon.leagueoflegends.com/cdn/9.21.1/img/champion/' + name + '.png')
+    .setThumbnail(loldomain + name + '.png')
     .addField('Classes', tags, true)
     .addField('Roles', roles, true)
     .addField('Builds', ['https://app.mobalytics.gg/champions/' + name.toLowerCase().replace(' ', '').replace("'", "") + '/build','https://champion.gg/champion/' + name.toLowerCase().replace(' ', '').replace("'", ""),'https://u.gg/lol/champions/' + name.toLowerCase().replace(' ', '').replace("'", "") + '/build'])
